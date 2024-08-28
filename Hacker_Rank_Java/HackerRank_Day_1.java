c14fd28b8e3815873e136d336a2de1c081d56b18
SDETabhayshukla <sdetabhay@gmail.com>
2024-08-28 19:14:11 +0530

    HackerRank Day 1 : Java Stdin and Stdout I

--- Hacker_Rank_Java/HackerRank_Day_1.java
+++ Hacker_Rank_Java/HackerRank_Day_1.java
@@ -12,8 +12,8 @@ public class HackerRank_Day_1 {
         //Method 1 : Using for Loop
         int n=3;
         for(int i=0; i<n;i++){
-            int num = scn.nextInt();
-            System.out.println(num);
+            int nums = scn.nextInt();
+            System.out.println(nums);
         }
 
         //Method 2 : Using While loop
