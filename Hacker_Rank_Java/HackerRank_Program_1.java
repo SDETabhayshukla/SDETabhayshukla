dae8c9c25b5365d688729df6e7f18f4ddabbb7bb
SDETabhayshukla <sdetabhay@gmail.com>
2024-08-29 19:09:59 +0530

    HackerRank Day 2 : 2 More Programs

--- Hacker_Rank_Java/HackerRank_Program_1.java
+++ Hacker_Rank_Java/HackerRank_Program_1.java
@@ -1,17 +1,31 @@
+/*
+Task
+In this challenge, you must read  integers from stdin and then print them to stdout.
+Each integer must be printed on a new line.
+Input Format
+There are  lines of input, and each line contains a single integer.
+Sample Input
+42
+100
+125
+Sample Output
+42
+100
+125
+*/
+
 package Hacker_Rank_Java;
 
 import java.util.Scanner;
 
-public class HackerRank_Day_1 {
-    //Java Stdin and Stdout I
+public class HackerRank_Program_1 {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner scn = new Scanner(System.in);
 
         //Method 1 : Using for Loop
-        int n=3;
-        for(int i=0; i<n;i++){
+        int n = 3;
+        for (int i = 0; i < n; i++) {
             int num = scn.nextInt();
             System.out.println(num);
         }
